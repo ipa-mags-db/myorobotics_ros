57b9c20b5b3a9c933014a2aa47671b174089e52d
Daniel Bargmann <daniel.bargmann@ipa.fraunhofer.de>
2016-11-16 13:30:12 +0100

    reworked to be a displacement Controller

--- myo_controllers/include/myo_controllers/testController.h
+++ myo_controllers/include/myo_controllers/testController.h
@@ -12,12 +12,20 @@ namespace myo_controllers {
 class testController : public baseController
 {
 public:
+
   pidControl pC1;
+  pidControl pC2;
+  compensateEMF emfChecker;
+  ffControl ff1;
+  angleNorm an1;
+  simpleLowPass lp1;
+
   int count;
 
-  testController() : pC1("name1"), pC2('name2') {}
+  testController() : pC1("PID1"), pC2("PID2"), ff1("FF1"), emfChecker(2000,200,20), an1("AN1"), lp1(0.2)  {}
   void loop();
   bool startup();
+
 };
 
 }
